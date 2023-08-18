0d74db984e80985151bdd27f3fe4f7d99f6d986d
linwenkang <linwenkang@meituan.com>
2023-08-18 10:04:51 +0800

    refactor: 更新配置信息

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports=  {
     "extends": [
         "react-app",
-        "react-app/jest"
+        "react-app/jest",
     ],
     "rules": {
         "semi": [2, "never"],
         "comma-dangle": ["error", "always-multiline"],
-        "indent": [2, 4]
-    }
+        "indent": [2, 4],
+        "import/no-cycle": 'error',
+    },
 }
